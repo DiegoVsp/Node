06667e50b7fd244a422f9dae8205084d2f8876ad
Diego Vespa <diego-vespa2020@outlook.com.br>
2020-10-21 16:39:55 -0300

    att

---
+++ aula06-cli/test.js
@@ -0,0 +1,28 @@
+const {
+  deepStrictEqual,
+  ok
+} = require('assert')
+
+const database = require('./database');
+const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'Speed', id: 1 }
+
+describe('Suite de manipulação de Herois', () => {
+  before(async () => {
+    await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+  })
+  it('deve pesquisar um Heroi usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR
+    // passando como destructor consegue pegar a 
+    // primeira posição sem precisar passar dessa maneira resultado[0]
+    const [resultado] = await database.listar(expected.id)
+    deepStrictEqual(resultado, expected)
+  })
+  it('deve cadastrar um heroi, usando arquivos', async () => {
+
+    const expected = DEFAULT_ITEM_CADASTRAR
+
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+    deepStrictEqual(actual, expected)
+  })
+})
